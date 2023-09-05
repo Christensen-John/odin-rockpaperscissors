bb1d7a21937f038202fb7607093ecd8289d3cbf5
Christensen-John <121368802+Christensen-John@users.noreply.github.com>
2023-09-05 17:08:43 +0100

    Add buttons and button logic

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,3 +1,37 @@
+/* || Variables and Data */
+const gameButtons = document.querySelectorAll("div.input>button");
+const scoreboard = document.querySelectorAll("div.score-window>h3:last-child");
+let playerScore = 0;
+let computerScore = 0;
+
+/* || Event Listener Wiring */
+gameButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    gameInput(e.target);
+  });
+});
+/*
+1. Listen for click
+2. Save player choice
+3. Get computer choice
+4. Compare using round()
+5. Round updates Scores
+*/
+
+/* || Functions */
+function gameInput(buttonPressed) {
+  let playerChoice = "rock";
+  switch (buttonPressed.className) {
+    case "paper":
+      playerChoice = "paper";
+      break;
+    case "scissors":
+      playerChoice = "scissors";
+      break;
+  }
+  round(playerChoice, getComputerChoice());
+}
+
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3);
   switch (choice) {
@@ -10,98 +44,68 @@ function getComputerChoice() {
   }
 }
 
-const sanitize = (playerInput) => {
-  if (typeof playerInput === "string") {
-    return playerInput.toLowerCase();
-  }
-  if (playerInput === null) {
-    return null;
-  }
-  alert("Player answered unclearly, using default value.");
-  return "rock";
-};
-// const choiceComparison = (player1Choice, player2Choice) => (
-
-// );
+//This function no longer needed since player no-longer types their choice
+// const sanitize = (playerInput) => {
+//   if (typeof playerInput === "string") {
+//     return playerInput.toLowerCase();
+//   }
+//   if (playerInput === null) {
+//     return null;
+//   }
+//   alert("Player answered unclearly, using default value.");
+//   return "rock";
+// };
 
 function round(playerSelection, computerSelection) {
-  playerSelection = sanitize(playerSelection);
+  //Sanitization no longer needed.
+  // playerSelection = sanitize(playerSelection);
   if (playerSelection === null) {
-    console.log("Cancel round");
+    // console.log("Cancel round");
   } else if (playerSelection === "rock") {
     if (computerSelection === "rock") {
-      console.log("You tie! You both played Rock!");
-      return 0;
+      // console.log("You tie! You both played Rock!");
     } else if (computerSelection === "paper") {
-      console.log("You lose! Paper beats Rock!");
-      return -1;
+      // console.log("You lose! Paper beats Rock!");
+      computerScore++;
     } else {
-      console.log("You win! Rock beats Scissors!");
-      return 1;
+      // console.log("You win! Rock beats Scissors!");
+      playerScore++;
     }
   } else if (playerSelection === "paper") {
     if (computerSelection === "rock") {
-      console.log("You win! Paper beats Rock!");
-      return 1;
+      // console.log("You win! Paper beats Rock!");
+      playerScore++;
     } else if (computerSelection === "paper") {
-      console.log("You tie! You both played paper!");
-      return 0;
+      // console.log("You tie! You both played paper!");
     } else {
-      console.log("You lose! Scissors beats Paper!");
-      return -1;
+      // console.log("You lose! Scissors beats Paper!");
+      computerScore++;
     }
   } else {
     if (computerSelection === "rock") {
-      console.log("You lose! Rock beats scissors");
-      return -1;
+      // console.log("You lose! Rock beats scissors");
+      computerScore++;
     } else if (computerSelection === "paper") {
-      console.log("You win! Scissors beats paper!");
-      return 1;
+      // console.log("You win! Scissors beats paper!");
+      playerScore++;
     } else {
-      console.log("You tie! You both played Scissors!");
-      return 0;
+      // console.log("You tie! You both played Scissors!");
     }
   }
+  updateScore();
+}
+
+function updateScore() {
+  scoreboard[0].textContent = playerScore;
+  scoreboard[1].textContent = computerScore;
 }
 
 function game() {
   let rounds = 0;
   let playerScore = 0;
   let computerScore = 0;
-  while (rounds < 5) {
-    let playerChoice = prompt(
-      "Type rock, paper, or scissors to choose your play"
-    );
-    let computerChoice = getComputerChoice();
-    let result = round(playerChoice, computerChoice);
-    switch (result) {
-      case -1:
-        computerScore++;
-        break;
-      case 1:
-        playerScore++;
-        break;
-      default:
-        break;
-    }
-    rounds++;
-  }
-  console.log(
-    `Results:
-        Player score: ` +
-      playerScore +
-      `
-        Computer Score: ` +
-      computerScore
-  );
-  let replay = confirm("Want to play again?");
-  if (replay) {
-    game();
-  }
 }
 
-// let playerChoice = 'rock';
-// let computerChoice = getComputerChoice();
-// console.log("player choice is: " + playerChoice + " and the computers choice is: " + computerChoice);
-// round(playerChoice, computerChoice);
+// || Setup the Game //
+
 game();
